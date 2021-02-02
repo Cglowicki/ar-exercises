b9a903e94e1d0a2b8be18d28a5ad0d67d239baed
Cameron <camglowicki@hotmail.com>
2021-02-02 08:17:37 +0000

    ex 5: get totals and averages

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+p total_revenue = Store.sum("annual_revenue")
+p avg_revenue = Store.average("annual_revenue").to_i
+p million_plus = Store.where("annual_revenue > ?", 1000000).count
